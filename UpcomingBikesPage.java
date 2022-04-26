bb582004dec7ac80f64bd97dc0cc81ffad790024
Indhu s <indhusparrow17@gmail.com>
2022-04-26 19:13:01 +0530

    thirdone

---
+++ UpcomingBikesPage.java
@@ -0,0 +1,11 @@
+package com.zigwheels.pages;
+
+import org.openqa.selenium.By;
+
+public class UpcomingBikesPage {
+
+	public static By ManufacturerDD = By.id("makeId");
+	public static By HondaBikeList = By.xpath("//ul[@id='modelList']/li");
+	public static By ViewMoreHondaBikeList = By.xpath("//*[@id='modelList']/li[16]/span");
+	
+}
---
+++ UsedCarsInChennaiPage.java
@@ -0,0 +1,11 @@
+package com.zigwheels.pages;
+
+import org.openqa.selenium.By;
+
+public class UsedCarsInChennaiPage {
+
+	public static By UsedCarsDD = By.linkText("Used Cars");
+	public static By UsedCarsInChennai = By.xpath("//a[@city='Chennai']");
+	public static By PopularModelList = By.xpath("//div[@class='gsc_thin_scroll']//ul[1]");
+
+}
